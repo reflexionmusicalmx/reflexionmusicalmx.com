0fcdf4a7aa41afb699bf992809531fa6c774098e
Reflexion Musical MX <paginawebreflexionmusicalmx@gmail.com>
2026-04-30 22:55:44 -0600

    Add music albums with track details and links
    Added two music albums with tracks and links to streaming services.

---
+++ datos.js
@@ -0,0 +1,151 @@
+{ 
+                name: "Hijos del olvido", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/hijos-del-olvido-1?in=reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "Empieza mal (Remix)", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/empieza-mal-remix?in=reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "Del dolor al odio", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/del-dolor-al-odio?in=reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "¿A qué costo?", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/a-qu-costo?in=reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "Eres lo que escuchas (Remix)", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/eres-lo-que-escuchas-remix?in=reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "Epílogo", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/epilogo-a-eres-lo-que-escuchas?in=reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            }
+        ],
+        links: {
+            youtube: "https://music.youtube.com/playlist?list=OLAK5uy_mRcy2Ln9I292iseBRcoyBCjedzlTS4X9I",
+            spotify: "https://open.spotify.com/album/20FnT6FbnltmFm9PiRSSkt?si=OD37ewXkRzyxJLLibBXmvQ",
+            amazon: "https://music.amazon.com.mx/albums/B0GD2PZZ1T",
+            apple: "https://music.apple.com/es/album/eres-lo-que-escuchas/1865017257",
+            soundcloud: "https://soundcloud.com/reflexionmusicalmx-music/sets/eres-lo-que-escuchas-1"
+        }
+    },
+    {
+        img: "images/Amor en tiempos de ideologias cancerigenas.webp",
+        title: "Amor en tiempos de ideologías cancerígenas",
+        tracks: [
+            { 
+                name: "ANTES DE AMAR", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/antes-de-amar-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "AMOR EN TIEMPOS DE IDEOLOGÍAS...", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/amor-en-tiempos-de-1?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "SILENCIO EN LA CAMA", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/silencio-en-la-cama-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "AUN DESPIERTO CONTIGO", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/aun-despierto-contigo-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "PERDÓNAME", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/perdoname-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "PERDÓN Y GRACIAS", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/perdon-y-gracias-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "JURAMENTO DE DOS", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/juramento-de-dos-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "JURAMENTO DE 2", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/juramento-de-2-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "HONRA A TU PAREJA", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/honra-a-tu-pareja-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "COMPLEMENTO ETERNO", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/complemento-eterno-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "AMOR CONSCIENTE", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/amor-consciente-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "AFIRMACIÓN", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/afirmacion-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "TIEMPO DE CALIDAD", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/tiempo-de-calidad-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "ACTOS DE SERVICIO", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/actos-de-servicio-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "REGALOS", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/regalos-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "CONTACTO FÍSICO", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/contacto-fisico-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "AMOR MADURO", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/amor-maduro-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "MARTE I", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/marte-i-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "MARTE II", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/marte-ii-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "VENUS I", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/venus-i-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "VENUS II", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/venus-ii-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "APRENDER A ORBITAR", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/aprender-a-orbitar-cumbia?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "PERDONENME (Bachata)", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/perdonenme-bachata?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "PERDON Y GRACIAS (Salsa)", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/perdon-y-gracias-salsa?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "AMOR EN TIEMPOS DE 2", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/amor-en-tiempos-de-2?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            },
+            { 
+                name: "COMPLEMENTO ETERNO (Vallenato)", 
+                url: "https://soundcloud.com/reflexionmusicalmx-music/complemento-eterno-vallenato?in=reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias&utm_source=clipboard&utm_medium=text&utm_campaign=social_sharing" 
+            }
+        ],
+        links: {
+            youtube: "https://music.youtube.com/playlist?list=OLAK5uy_kP7_3VPWPlsoggwsXy0gQ6ZFFmv9drSIA",
+            spotify: "https://open.spotify.com/album/5bouzliTlE1sm3qaIyEO8E?si=cs4bQlYESxiFc4mzFpsX6A",
+            amazon: "https://music.amazon.com.mx/albums/B0GM2D1VFK",
+            apple: "https://music.apple.com/es/album/amor-en-tiempos-de-ideolog%C3%ADas-cancer%C3%ADgenas/1875409405",
+            soundcloud: "https://soundcloud.com/reflexionmusicalmx-music/sets/amor-en-tiempos-de-ideologias"
+        }
+    }
+];
